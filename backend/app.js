60d7c05a37289bfe0ed3d8ad14e38e4fd6621a1a
Shubha Sarkar <sarkarshubha123@gmail.com>
2024-10-07 20:37:42 +0530

    allow accesscontrol;

--- backend/app.js
+++ backend/app.js
@@ -7,13 +7,22 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT;
 
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+  });
+
 app.use(
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173", 
     credentials: true, // Allow cookies and headers if you're using them
-   
   })
 );
+
+
 
 app.use(express.json());
 
@@ -109,9 +118,7 @@ function authUser(...userRoles) {
 }
 
 //connect to the mongoose:
-mongoose.connect(process.env.DB_URL, {
-  useNewUrlParser: true,
-});
+mongoose.connect(process.env.DB_URL);
 
 //admin signup 🍏
 
